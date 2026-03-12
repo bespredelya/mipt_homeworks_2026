ec5b0e6457a9afbd173ac361c6024bfa289c753b
Matvey Skirda <skirdamatvei@yandex.ru>
2026-03-12 04:30:42 +0300

    Add homework for types and functions

---
+++ Part_3_Types_Conditions_Loops_Functions/hw_3.py
@@ -0,0 +1,32 @@
+UNKNOWN_COMMAND_MSG = "Неизвестная команда!"
+NONPOSITIVE_VALUE_MSG = "Значение должно быть больше нуля!"
+INCORRECT_DATE_MSG = "Неправильная дата!"
+OP_SUCCESS_MSG = "Добавлено"
+
+
+def is_leap_year(year: int) -> bool:
+    """
+    Для заданного года определяет: високосный (True) или невисокосный (False).
+
+    :param int year: Проверяемый год
+    :return: Значение високосности.
+    :rtype: bool
+    """
+
+
+def extract_date(maybe_dt: str) -> tuple[int, int, int] | None:
+    """
+    Парсит дату формата DD-MM-YYYY из строки.
+
+    :param str maybe_dt: Проверяемая строка
+    :return: typle формата (день, месяц, год) или None, если дата неправильная.
+    :rtype: tuple[int, int, int] | None
+    """
+
+
+def main():
+    """Ваш код здесь"""
+
+
+if __name__ == "__main__":
+    main()
